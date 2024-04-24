8749df7dfb18eba2196298a7858c73fabd9d9a92
rahul008xy <158485654+rahul008xy@users.noreply.github.com>
2024-04-25 03:52:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
 
-    AUTH_USERS = os.environ.get('AUTH_USERS', '5881684718').split(',')
+    AUTH_USERS = os.environ.get('AUTH_USERS', '-1002072973539').split(',')
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '-4184370559').split(',')
+    GROUPS = os.environ.get('GROUPS', '-1002072973539').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
