df2e1a0cff3418455e900ec3006bc28b7a90feef
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-02 14:09:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("https://DRMbot-new-mst-for-render.onrender.com/")
+    return web.json_response("https://drmbot-new-mst.onrender.com/")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
